887cb5c76d8a8217cdc7b728e24b889ef140a02a
Uzair Khan <uzairkhan60621@gmail.com>
2026-09-11 23:23:08 +0500

    Adding the complete structure of calculator

--- react-calculator/src/App.jsx
+++ react-calculator/src/App.jsx
@@ -11,8 +11,10 @@ function App() {
     );
   };
 
-  const inputhandler = (num1, num2) => {
-    // const 
+  const inputOperator = (operator) => {
+    setDisplay((previous) =>
+      previous === "0" ? operator : previous + operator
+    );
   }
   
   return (
@@ -22,6 +24,16 @@ function App() {
         <button onClick={() => handleNumber("7")}>7</button>
         <button onClick={() => handleNumber("8")}>8</button>
         <button onClick={() => handleNumber("9")}>9</button>
+        <button onClick={() => inputOperator("+")}>+</button><br />
+        <button onClick={() => handleNumber("6")}>6</button>
+        <button onClick={() => handleNumber("5")}>5</button>
+        <button onClick={() => handleNumber("4")}>4</button>
+        <button onClick={() => inputOperator("-")}>-</button><br />
+        <button onClick={() => handleNumber("3")}>3</button>
+        <button onClick={() => handleNumber("2")}>2</button>
+        <button onClick={() => handleNumber("1")}>1</button>
+        <button onClick={() => inputOperator("*")}>*</button><br />
+        <button onClick={() => inputOperator("/")}>/</button>
       </div>
     </div>
   )
