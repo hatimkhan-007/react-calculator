2169a72697d7795b0d756aed171209558c19ef3a
Uzair Khan <uzairkhan60621@gmail.com>
2026-09-11 23:12:11 +0500

    Adding the input handler function which handle the operator and the 2nd input and then result

--- react-calculator/src/App.jsx
+++ react-calculator/src/App.jsx
@@ -4,12 +4,24 @@ import { useState } from 'react';
 
 function App() {
   const [display, setDisplay] = useState("0");
+
+  const handleNumber = (number) => {
+    setDisplay((previous) => 
+      previous === "0" ? number :previous + number
+    );
+  };
+
+  const inputhandler = (num1, num2) => {
+    // const 
+  }
   
   return (
     <div className="app">
       <div className="display">
         <h1>{display}</h1>
-        <button onClick={() => setDisplay("7")}>7</button>
+        <button onClick={() => handleNumber("7")}>7</button>
+        <button onClick={() => handleNumber("8")}>8</button>
+        <button onClick={() => handleNumber("9")}>9</button>
       </div>
     </div>
   )
